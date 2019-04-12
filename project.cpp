a9deeabbee67d0f71839f774cc93bdc141a3adff
SHASHWAT KUMAR <500068392@DDN.UPES.AC.IN>
2019-04-12 15:03:58 +0530

    Struct Node, insert_new_node_beg(), insert_new_node_end() functions added

--- project.cpp
+++ project.cpp
@@ -3,6 +3,71 @@
 using namespace std;
 
 
+struct Node
+{
+    int sap;
+    int rollno;
+    struct Node *next;
+};
+
+struct Node *head =NULL;
+
+
+void insert_new_node_beg()
+{
+    struct Node *new_node = new struct Node();
+
+    cout<<"Enter the student's SAP ID(last 5 digits) :"<<endl;
+    int x;
+    cin>>x;
+    cout<<"\n\n"<<endl;
+    new_node->sap=x;
+
+    cout<<"Enter student's roll number(last 3 digits) :"<<endl;
+    cin>>x;
+    cout<<"\n\n"<<endl;
+    new_node->rollno=x;
+
+    new_node->next =NULL;
+
+    new_node->next=head;
+    head=new_node;
+
+}
+
+
+
+void insert_new_node_end()
+{
+    struct Node *new_node = new struct Node();
+
+    cout<<"Enter the student's SAP ID(last 5 digits) :"<<endl;
+    int x;
+    cin>>x;
+    cout<<"\n\n"<<endl;
+    new_node->sap=x;
+
+    cout<<"Enter student's roll number(last 3 digits) :"<<endl;
+    cin>>x;
+    cout<<"\n\n"<<endl;
+    new_node->rollno=x;
+
+    new_node->next =NULL;
+
+    struct Node *temp;
+    temp=head;
+
+    while(temp->next != NULL)
+    {
+        temp= temp->next;
+    }
+
+    temp->next =new_node;
+
+}
+
+
+
 int main()
 {
 	return 0;
