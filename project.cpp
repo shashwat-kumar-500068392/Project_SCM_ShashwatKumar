bece6761a6283d11b977d7855dcc336423b1b7b0
nit29sin <nitish29singla@gmail.com>
2019-04-21 22:25:42 +0530

    create_list(),add_begin() and add_after() functions added -- nitish

--- project.cpp
+++ project.cpp
@@ -314,6 +314,44 @@ void add_after(int value, int pos)
     cout<<"Element Inserted"<<endl;
 }
 
+void add_after(int value, int pos)
+{
+    if (start == NULL)
+    {
+        cout<<"First Create the list."<<endl;
+        return;
+    }
+    struct node *tmp, *q;
+    int i;
+    q = start;
+    for (i = 0;i < pos - 1;i++)
+    {
+        q = q->next;
+        if (q == NULL)
+        {
+            cout<<"There are less than ";
+            cout<<pos<<" elements."<<endl;
+            return;
+        }
+    }
+    tmp = new(struct node);
+    tmp->info = value;
+    if (q->next == NULL)
+    {
+        q->next = tmp;
+        tmp->next = NULL;
+        tmp->prev = q;
+    }
+    else
+    {
+        tmp->next = q->next;
+        tmp->next->prev = tmp;
+        q->next = tmp;
+        tmp->prev = q;
+    }
+    cout<<"Element Inserted"<<endl;
+}
+
 
 
 
